bf971e8bb5b8799ed4cc1dedd7f681cce349e047
zhangshuaibao922 <zhangshuaibao922@163.com>
2023-03-09 12:41:05 +0800

    frist1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
     }
 }
